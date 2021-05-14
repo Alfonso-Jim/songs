46ae9011e6e2c8a019174954abfb051f023150f2
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-14 14:56:39 +0200

    added reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "Keep Moving",
+      duration: "3:05",
+    },
+    {
+      title: "Casio",
+      duration: "2:45",
+    },
+    {
+      title: "Julia",
+      duration: "4:15",
+    },
+    {
+      title: "Magnolia",
+      duration: "2:15",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
